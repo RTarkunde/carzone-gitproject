f3ba7c3da6380cfe3165b3c2558cc99818585ff0
Samarth Tarkunde <rtarkund@yahoo.com>
2022-02-09 09:45:31 +0530

    Checkpoint

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')# Create your views here.
+
+def about(request):
+    return render(request, 'pages/about.html')# Create your views here.
+
+def services(request):
+    return render(request, 'pages/services.html')# Create your views here.
+
+def contact(request):
+    return render(request, 'pages/contact.html')# Create your views here.
+
+def IT104pc(request):
+    return render(request, 'pages/IT104pc.html')# Create your views here.
